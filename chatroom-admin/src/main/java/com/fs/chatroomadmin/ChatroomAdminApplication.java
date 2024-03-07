98da413e8374709d009fbcfd6ac8dbfaed9178f2
chenziwen <chenziwen0413@163.com>
2024-03-07 15:03:43 +0800

    整理项目1.1

---
+++ chatroom-admin/src/main/java/com/fs/chatroomadmin/ChatroomAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fs.chatroomadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomAdminApplication.class, args);
+	}
+
+}
